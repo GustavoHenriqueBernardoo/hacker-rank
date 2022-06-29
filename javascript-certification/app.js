b9866f0e86b9a718e132da46978f7cd0790e2cab
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-06-29 11:42:40 -0300

    javascript cert

--- javascript-certification/app.js
+++ javascript-certification/app.js
@@ -1,16 +1,6 @@
 // ==== Fizz Buzz
 
 function fizzBuzz(n) {
-  // Write your code here
-  // for (let i = 1; i < n; i++) {
-  //   if (i % 5 === 0 && i % 3 === 0) {
-  //     return i
-  //   } else if (i % 5 === 0 && i % 3 !== 0) {
-  //     return 'Buzz'
-  //   } else if (i % 3 === 0 && i % 5 !== 0) {
-  //     return 'Fizz'
-  //   }
-  // }
   for (let i = 1; i <= n; i++) {
     if (i % 5 > 0 && i % 3 > 0) {
       // console.log(i)
@@ -44,6 +34,8 @@ function stripProperty(obj, prop) {
 
 // console.log(stripProperty({ foo: 2, bar: 3, baz: 3 }, 'foo'))
 
+// === WeekDay and getText
+
 function weekdayText(weekdays) {
   return function getText(index) {
     if (index > weekdays.length) {
@@ -54,4 +46,36 @@ function weekdayText(weekdays) {
 }
 
 
-console.log(weekdayText(['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']))
+// console.log(weekdayText(['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']))
+
+
+class SiegeState {
+  constructor() {
+    this.canMove = false
+    this.damage = 20
+  }
+}
+
+class TankState {
+  constructor() {
+    this.canMove = true
+    this.damage = 5
+  }
+}
+
+class Tank {
+  constructor() {
+    this.state = new TankState
+  }
+
+  get canMove() {
+    return this.state.canMove
+  }
+  get damage() {
+    return this.state.damage
+  }
+}
+
+const test = new Tank()
+
+console.log(test)
