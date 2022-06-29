8609835e55d6d35d3f871cc715f89975eb912d46
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-06-29 11:41:54 -0300

    javascript practice

--- Problem Solving-Algorithms and DataStructures/javascript/app.js
+++ Problem Solving-Algorithms and DataStructures/javascript/app.js
@@ -1,1 +1,21 @@
 console.log('works')
+
+function compareTriplets(a, b) {
+  // Write your code here
+  let resultA = 0
+  let resultB = 0
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === b[i]) {
+      continue
+    }
+    if (a[i] > b[i]) {
+      resultA++
+    } else {
+      resultB++
+    }
+  }
+  let INTEGER_ARRAY = [resultA, resultB]
+  return INTEGER_ARRAY
+}
+
+console.log(compareTriplets([5, 2, 3], [4, 1, 2]))
